436bb78758d694b71b1fe70af4eed2b0a23498f5
fatemehkhoramdel <111147876+fatemehkhoramdel@users.noreply.github.com>
2024-10-29 18:11:49 +0330

    footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import "./App.css";
 
-import './App.css';
-
-export default  function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Weather app
-      </header>
+      <header className="App-header">Weather app</header>
+
+      <footer>
+        <p>
+          This project is{" "}
+          <a href="https://github.com/fatemehkhoramdel/weatherappreact.git">
+            {" "}
+            open source
+          </a>{" "}
+          and creat by Ftaemeh Khoramdel
+        </p>
+      </footer>
     </div>
   );
 }
-
-
